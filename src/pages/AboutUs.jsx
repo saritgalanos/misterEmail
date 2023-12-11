846a3456c21f32553b6ae8f24f9e2efc91425790
Sarit Galanos <saritgalanos@gmail.com>
2023-12-11 10:00:23 +0200

    testing github

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,6 @@
 export function AboutUs() {
     return (
+        /*learning github*/
         <div className="about">
             <h1>We are all about emails</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus suscipit quibusdam deleniti corrupti quasi, temporibus porro pariatur quos nisi aspernatur, alias totam hic. Nobis, cupiditate veritatis? Similique cum corporis tempore?</p>
@@ -8,6 +9,7 @@ export function AboutUs() {
                 <Link to="/about/vision">Vision</Link>
             </nav>
             <Outlet /> */}
+           
         </div>
     )
 }
